ea176820cb3cc30300c0a05d1e5daec6e6449acd
Dumi92 <33468753+Dumi92@users.noreply.github.com>
2017-11-17 14:22:42 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
 <!doctype html>
+    var express = require('express')
+var app = express()
+
+app.set('port', (process.env.PORT || 5000))
+app.use(express.static(__dirname + '/public'))
+
+app.get('/', function(request, response) {
+  response.send('Hello World!')
+})
+
+app.listen(app.get('port'), function() {
+  console.log("Node app is running at localhost:" + app.get('port'))
+})
+
 <html class="no-js" lang="">
     <head>
         <meta charset="utf-8">
